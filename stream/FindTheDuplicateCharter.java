c399c71af459fe4ccce83b7121db8f87239d6b56
ranjith5607 <anderanjith@gmail.com>
2024-02-02 00:24:19 +0530

    FindTheDuplicateCharter Updated

--- stream/FindTheDuplicateCharter.java
+++ stream/FindTheDuplicateCharter.java
@@ -8,10 +8,9 @@ import java.util.stream.Collectors;
 public class FindTheDuplicateCharter {
 
 	public static void main(String[] args) {
+
+
 		String input = "civicAA";
-		StringBuilder stringBuilder = new StringBuilder(input);
-		String s1 = stringBuilder.reverse().toString();
-		System.out.println(s1.equals(input));
 
 		Map<Character, Long> collect = 
 				input.chars().mapToObj(s -> Character.valueOf((char)s))
